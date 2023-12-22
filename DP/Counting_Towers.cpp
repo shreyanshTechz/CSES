4714c507d8a12b7822fd913e2624e162534daab4
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2023-12-22 21:43:12 +0530

    Counting Towers

--- DP/Counting_Towers.cpp
+++ DP/Counting_Towers.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define mod 1000000007
+
+
+
 int main()
 {
     int t;
     cin >> t;
-
+    vector<vector<int>> dp(1e6+1, vector<int>(2));
     while (t--)
     {
         int n;
         cin >> n;
 
-        vector<vector<int>> dp(n + 1, vector<int>(2));
-
         dp[n][0] = 1;
         dp[n][1] = 1;
 
-        for (int i = n - 1; i >= 1; i--)
+        for (int i = n - 1; i >= 0; i--)
         {
-            dp[i][0] += (2LL * dp[i + 1][0] + dp[i + 1][1]) % mod;
-            dp[i][1] += (4LL * dp[i + 1][1] + dp[i + 1][0]) % mod;
+            dp[i][0] = (2LL * dp[i + 1][0] + dp[i + 1][1]) % mod;
+            dp[i][1] = (4LL * dp[i + 1][1] + dp[i + 1][0]) % mod;
         }
 
         cout << (dp[1][0] + dp[1][1]) % mod << "\n";

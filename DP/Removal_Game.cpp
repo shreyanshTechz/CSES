3039a8f66e2d9cd0f3e74ca4bebd8a35788eaa68
Shreyansh Pandey <shreyansh.fractal@gmail.com>
2023-12-29 19:47:59 +0530

    solution corrected

--- DP/Removal_Game.cpp
+++ DP/Removal_Game.cpp
@@ -5,33 +5,28 @@ int main()
 {
     int n;
     cin >> n;
-    vector<ll> arr(n), pre(n);
-    for (int i = 0; i < n; i++)
+    vector<ll> arr(n+1), pre(n+1);
+    for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
     }
-    pre[0] = arr[0];
-    for (int i = 1; i < n; i++)
+    
+    int dp[n + 1][n + 1];
+    for (int i = 1; i <= n; i++)
     {
         pre[i] = pre[i - 1] + arr[i];
-    }
-
-    int dp[n + 1][n + 1];
-
-    for (int i = 0; i < n; i++)
-    {
         dp[i][i] = arr[i];
     }
 
-    dp[n][n] = 0;
-
-    for (int i = 1; i < n; i++)
+    for (int j = 1; j < n; j++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int i = 1; i <= n; i++)
         {
-            dp[i][j] = max(pre[j] - pre[i-1] - dp[i+1][j],pre[j] - pre[i-1] - dp[i][j-1]);
+            if(i+j > n) break;
+            dp[i][j+i] = max(pre[j+i] - pre[i-1] - dp[i+1][j+i],pre[j+i] - pre[i-1] - dp[i][j+i-1]);
         }
+        
     }
 
-    cout<<dp[0][n-1];
+    cout<<dp[1][n];
 }
